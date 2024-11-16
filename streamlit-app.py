392592bf8559dd78b72edd63c6876b1f577931d0
Siarhei A <sergey.anfilets@contexxt.com>
2024-11-16 07:20:54 +0300

    icon assist

--- streamlit-app.py
+++ streamlit-app.py
@@ -16,7 +16,7 @@ ICON_USER = "\U0001F5E8"
 ICON_ASSISTANT = "\U0001F469"
 
 # Base st config
-st.beta_set_page_config(page_title='DIVA AI', 
+st.set_page_config(page_title='DIVA AI', 
                         page_icon="assets/logo.png", 
                         layout='wide', 
                         initial_sidebar_state='auto')
